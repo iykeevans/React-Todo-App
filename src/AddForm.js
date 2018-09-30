42143ec0d4072545e77328825574738564eda962
iykeevans <elochi238@gmail.com>
2018-09-30 23:55:07 +0100

    updated the state so that text inputed to addform components are reset

--- src/AddForm.js
+++ src/AddForm.js
@@ -12,13 +12,16 @@ export default class AddForm extends Component {
     handleSubmit = (e) => {
         e.preventDefault()
         this.props.addTodo(this.state);
+        this.setState({
+            content: ''
+        })
     }
   render() {
     return (
       <div>
         <form onSubmit={this.handleSubmit}>
             <label>Add new todo:</label>
-            <input type="text" onChange={this.handleChange}/>
+            <input type="text" onChange={this.handleChange} value={this.state.content}/>
         </form>
       </div>
     )
